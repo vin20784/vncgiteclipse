074a8b2c74cd7290bfad5817644ecf0bf3ab25e2
MLE4 <mitalandge@gmail.com>
2019-07-04 16:46:36 +0530

    data retrive mechanism change

--- routes/service.js
+++ routes/service.js
@@ -34,7 +34,7 @@ const getData = async function (iURL, iArgs) {
   return irData.body;
 }
 
-const postData = async function(iURL, iArgs){
+const postData = async function (iURL, iArgs) {
 
   let authData = await authManager.getAuthenticationData();
   var options = getPostOption(authData, iURL, iArgs);
@@ -43,37 +43,95 @@ const postData = async function(iURL, iArgs){
   return irData.body;
 }
 
+const putData = async function (iURL, iArgs) {
+
+  let authData = await authManager.getAuthenticationData();
+  var options = getPostOption(authData, iURL, iArgs);
+  let irData = await reqManager.putRequest(options);
+
+  return irData.body;
+}
+
 /* GET home page. */
 router.post('/dsx/get', function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  let args = req.body;
-  let URL = req.body.url;
-  delete args['url'];
-  if(URL == "" || URL == undefined){
-    res.json(JSON.stringify({"message": "Wrong URL"}));
-  }else if(URL.indexOf('ppd') < 0){
-    res.json(JSON.stringify({"message":"Please use a right pre-production URL"}));
-  }else{
-    getData(URL, args).then(function (iData) {
-      res.json(iData);
-    }).catch(function(iReason){
-      res.json(JSON.parse(iReason));
+  if (req.method == 'POST') {
+    let body = '';
+    req.on('data', function (data) {
+      body += data;
+      if (body.length > 1e6) {
+        req.connection.destroy();
+      }
+    });
+    req.on('end', function () {
+      let args = JSON.parse(body);
+      let URL = args.url;
+      delete args['url'];
+      if (URL == "" || URL == undefined) {
+        res.json(JSON.stringify({ "message": "Wrong URL" }));
+      } else if (URL.indexOf('ppd') < 0) {
+        res.json(JSON.stringify({ "message": "Please use a right pre-production URL" }));
+      } else {
+        getData(URL, args).then(function (iData) {
+          res.json(iData);
+        }).catch(function (iReason) {
+          res.json(JSON.parse(iReason));
+        });
+      }
     });
   }
 });
 
 router.post('/dsx/post', function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  let args = req.body;
-  let URL = req.body.url;
-  delete args['url'];
-  if(URL == "" || URL == undefined){
-    res.json({"message": "Wrong URL"});
-  }else{
-    postData(URL, args).then(function (iData) {
-      res.json(iData);
-    }).catch(function(iReason){
-      res.json(JSON.parse(iReason));
+  if (req.method == 'POST') {
+    let body = '';
+    req.on('data', function (data) {
+      body += data;
+      if (body.length > 1e6) {
+        req.connection.destroy();
+      }
+    });
+    req.on('end', function () {
+      let args = JSON.parse(body);
+      let URL = args.url;
+      delete args['url'];
+      if (URL == "" || URL == undefined) {
+        res.json({ "message": "Wrong URL" });
+      } else {
+        postData(URL, args).then(function (iData) {
+          res.json(iData);
+        }).catch(function (iReason) {
+          res.json(JSON.parse(iReason));
+        });
+      }
+    });
+  }
+});
+
+router.post('/dsx/put', function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  if (req.method == 'POST') {
+    let body = '';
+    req.on('data', function (data) {
+      body += data;
+      if (body.length > 1e6) {
+        req.connection.destroy();
+      }
+    });
+    req.on('end', function () {
+      let args = JSON.parse(body);
+      let URL = args.url;
+      delete args['url'];
+      if (URL == "" || URL == undefined) {
+        res.json({ "message": "Wrong URL" });
+      } else {
+        putData(URL, args).then(function (iData) {
+          res.json(iData);
+        }).catch(function (iReason) {
+          res.json(JSON.parse(iReason));
+        });
+      }
     });
   }
 });
