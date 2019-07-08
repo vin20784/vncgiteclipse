5dd1d6d8a43f7bccd9c539bd54f5608fe9129dcf
Naveen Kumar <nkr4@3ds.com>
2019-07-08 18:00:39 +0530

    [Config] - config file changed

--- auth/config.js
+++ auth/config.js
@@ -2,5 +2,5 @@ module.exports = {
     username: "nkr4",
     password: "let@h3rknow",
     currentUrl: "https://dsxdev-online-ppd.dsone.3ds.com/enovia/rest/ws/v1/irs",
-    passPortURL: "https://euw1-devprol14-iam.3dx-staging.3ds.com/cas"
+    passPortURL: "https://euw1-devprol15-iam.3dx-staging.3ds.com/cas"
 }
